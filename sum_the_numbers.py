2616dbae17209695230864425cbc85da2c2fa306
Alan Jordan <alan.jordanbiz@gmail.com>
2020-01-17 22:54:12 -0500

    Sum the Numbers |List Exercise| Initial Commit

---
+++ sum_the_numbers.py
@@ -0,0 +1,13 @@
+list = [1,2,3,4,5,6,7,8,9]
+print(sum(list))
+
+# below I used a "for loop" to sum the list.
+
+# num_sum = 0
+
+# for i in range(len(list)):
+#     if list[i] > 0:
+#         add_to = list[i]
+#     num_sum = num_sum + add_to
+
+# print(num_sum)
