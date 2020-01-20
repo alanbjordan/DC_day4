cc4acaf23131b9c5b23c7776fc1fabe28ac498a1
Alan Jordan <alan.jordanbiz@gmail.com>
2020-01-20 10:47:11 -0500

    |While Loop Exercise| Print a Triangle Initial Commit

---
+++ print_a_triangle.py
@@ -0,0 +1,15 @@
+# Print a Triangle
+# Print a triangle consisting of * 
+j = 0
+
+while j < 4:
+    if j == 0:
+        print("   * ")
+    if j == 1:
+        print("  ***")
+    if j == 2:
+        print(" *****")
+    if j == 3:
+        print("*******")    
+    j += 1
+ 
